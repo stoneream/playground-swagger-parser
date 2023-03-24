97a327bac2a91e45a3fc96fe13b6ee0488c9b418
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2023-03-24 14:49:26 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import SwaggerParser from "@apidevtools/swagger-parser";
+import { OpenAPIV3_1 } from "openapi-types";
 
 try {
-  const api = await SwaggerParser.parse('./spotify-web-api/fixed-spotify-open-api.yml');
-  console.log("API name: %s, Version: %s", api.info.title, api.info.version);
+  await SwaggerParser.parse('./spotify-web-api/fixed-spotify-open-api.yml')
+    .then((document: OpenAPIV3_1.Document): void => {
+      console.log(document.components);
+    })
+    .then((_): void => {
+      console.error("unsupported version");
+    });
+
 }
 catch (err) {
   console.error(err);
