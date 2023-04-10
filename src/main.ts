3228eb184f276907da853922b5ecdbcaf3ff29b9
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2023-04-11 08:41:12 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,30 @@
 import SwaggerParser from "@apidevtools/swagger-parser";
+import { OpenAPIV3 } from "openapi-types";
 import { OpenAPIV3_1 } from "openapi-types";
 
 const path = './spotify-web-api/fixed-spotify-open-api.yml';
+
+const kebabToCamel = (s: string): string => s.replace(/-./g, (ss) => ss.charAt(1).toUpperCase());
+
+const handleOperation = (
+  httpMethod: OpenAPIV3_1.HttpMethods,
+  path: string,
+  operationObject?: OpenAPIV3_1.OperationObject
+): void => {
+
+  if (operationObject) {
+    if (operationObject.operationId) {
+      // console.log(kebabToCamel(operationObject.operationId));
+    }
+    // parameter
+    //    query
+    //    body
+    //    path
+    // response
+    //    ok
+    //    error
+  }
+}
 
 try {
   await SwaggerParser.parse(path)
@@ -9,8 +32,8 @@ try {
       console.log(document.components.schemas);
 
       const components = document.components;
-      const schemas = components.schemas;
 
+      const schemas = components.schemas;
       for (const [schemaName, schema] of Object.entries(schemas)) {
         const properties = schema.properties
 
@@ -40,6 +63,15 @@ try {
           }
         }
       }
+
+      const paths = document.paths
+      for (const [path, pathItem] of Object.entries(paths)) {
+        handleOperation(OpenAPIV3.HttpMethods.GET, path, pathItem.get);
+        handleOperation(OpenAPIV3.HttpMethods.POST, path, pathItem.post);
+        handleOperation(OpenAPIV3.HttpMethods.PUT, path, pathItem.put);
+        handleOperation(OpenAPIV3.HttpMethods.DELETE, path, pathItem.delete);
+      }
+
     });
 } catch (err) {
   console.error(err);
