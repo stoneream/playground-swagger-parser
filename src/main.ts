d19ba84e6250a8e4af9632f3d097541931ae94ad
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2023-04-12 23:52:40 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,29 @@ const path = './spotify-web-api/fixed-spotify-open-api.yml';
 
 const kebabToCamel = (s: string): string => s.replace(/-./g, (ss) => ss.charAt(1).toUpperCase());
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isReferenceObject = (property: any): property is OpenAPIV3_1.ReferenceObject =>
+  property.$ref !== undefined;
+
+const resolveType = (schema: OpenAPIV3_1.SchemaObject): string => {
+  if (schema.type) {
+    switch (schema.type) {
+      case 'string':
+        return 'String';
+      case 'integer':
+        return 'Int';
+      case 'boolean':
+        return 'Boolean';
+      case 'array':
+        return `List[${resolveType(schema.items as OpenAPIV3_1.SchemaObject)}]`;
+      default:
+        return 'String';
+    }
+  } else {
+    return 'String';
+  }
+}
+
 const handleOperation = (
   httpMethod: OpenAPIV3_1.HttpMethods,
   path: string,
@@ -15,11 +38,28 @@ const handleOperation = (
   if (operationObject) {
     if (operationObject.operationId) {
       // console.log(kebabToCamel(operationObject.operationId));
+      const operationId = kebabToCamel(operationObject.operationId)
+
+      const queryParameters = operationObject.parameters.filter((parameter: OpenAPIV3_1.ParameterObject) => parameter.in == 'query');
+      const pathParameters = operationObject.parameters.filter((parameter: OpenAPIV3_1.ParameterObject) => parameter.in == 'path');
+
+      const f = (parameter: OpenAPIV3_1.ParameterObject) => { }
+
+      const body = operationObject.requestBody;
+      if (isReferenceObject(body)) {
+        // do nothing
+      } else {
+        // do nothing
+        const mediaTypeObject = body.content.applicationJson.schema;
+
+        if (isReferenceObject(mediaTypeObject)) {
+          // do nothing
+        } else {
+          mediaTypeObject
+        }
+      }
     }
-    // parameter
-    //    query
-    //    body
-    //    path
+
     // response
     //    ok
     //    error
@@ -42,14 +82,8 @@ try {
           for (const [propertyName, property] of Object.entries(properties)) {
             console.log(propertyName, property)
 
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            const isReferenceObject = (property: any): property is OpenAPIV3_1.ReferenceObject =>
-              property.$ref !== undefined;
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            const isSchemaObject = (property: any): property is OpenAPIV3_1.SchemaObject =>
-              !isReferenceObject(property)
 
-            if (isSchemaObject(property)) {
+            if (!isReferenceObject(property)) {
               if (property.allOf) {
                 // do nothing
               } else {
